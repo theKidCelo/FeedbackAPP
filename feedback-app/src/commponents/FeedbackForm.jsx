1c9fe8a14aaf4cd788434a55e2888c5e0a80dc94
Ramy Mohamed <ramy.opene@gmail.com>
2023-03-26 00:30:16 -0400

    add validation to button submit

--- feedback-app/src/commponents/FeedbackForm.jsx
+++ feedback-app/src/commponents/FeedbackForm.jsx
@@ -4,8 +4,22 @@ import Button from "./share/Button"
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+
+        if (text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        }else if(text !== '' && text.trim().length <= 10) {
+            setMessage('Text must be atleast 10 characters')
+            setBtnDisabled(true)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
         setText(e.target.value)
     }
   return (
@@ -15,8 +29,10 @@ function FeedbackForm() {
 
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
-                <Button className="submit">Send</Button>
+                <Button className="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
+
+            {message && <div className="message">{message}</div>}
             </form>
     </Card>
   )
