be0a12c2c8ec4454ad445161df246288e95eb203
Ramy Mohamed <ramy.opene@gmail.com>
2023-03-26 07:04:03 -0400

    clean up code with pritter

--- feedback-app/src/pages/About.jsx
+++ feedback-app/src/pages/About.jsx
@@ -1,18 +1,18 @@
-import Card from "../commponents/share/Card"
-import {Link} from 'react-router-dom'
+import Card from "../commponents/share/Card";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
     <Card>
-    <div className="about">
+      <div className="about">
         <h1>The About Page</h1>
         <p>This is a small project I astarted to help learn react</p>
         <p>
-            <Link to="/">Back To Main Page</Link>
+          <Link to="/">Back To Main Page</Link>
         </p>
-    </div>
+      </div>
     </Card>
-  )
+  );
 }
 
-export default About
+export default About;
