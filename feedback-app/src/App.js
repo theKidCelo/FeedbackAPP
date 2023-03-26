5a3b1c790738306f93182328df353efdf4f852d5
Ramy Mohamed <ramy.opene@gmail.com>
2023-03-25 23:03:18 -0400

    add delete feature

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -6,11 +6,17 @@ import FeedbackData from "./data/Feedbackdata"
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const deleteFeedback = (id => {
+        if(window.confirm('Are you sure you want to delete')){
+        setFeedback(feedback.filter((item) => item.id !== id
+        ))
+        }
+    })
   return (
     <>
     <Header />
     <div className="container">
-        <Feedbacklist feedback = {feedback} />
+        <Feedbacklist feedback = {feedback} handleDelete={deleteFeedback}/>
     </div>
     </>
   )
--- feedback-app/src/commponents/FeedbackItem.jsx
+++ feedback-app/src/commponents/FeedbackItem.jsx
@@ -1,9 +1,15 @@
+import {FaTimes} from 'react-icons/fa'
+import PropTypes from 'prop-types'
 import Card from "./share/Card"
 
-function FeedbackItem({item}) {
+
+function FeedbackItem({item, handleDelete}) {
   return (
-    <Card >
+    <Card reverse={true}>
         <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className="close">
+            <FaTimes color="purlple" />
+        </button>
         <div className="text-display">{item.text}</div>
       
     </Card>
