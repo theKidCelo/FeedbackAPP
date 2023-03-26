46c2221d9fd4dab5e2276ab6dca114ef370aa8e9
Ramy Mohamed <ramy.opene@gmail.com>
2023-03-26 04:54:29 -0400

    add animations: fade in/fade out on new or deleted rating

--- feedback-app/src/commponents/Feedbacklist.jsx
+++ feedback-app/src/commponents/Feedbacklist.jsx
@@ -1,19 +1,30 @@
-import FeedbackItem from './FeedbackItem'
+import { motion, AnimatePresence } from "framer-motion";
+import FeedbackItem from "./FeedbackItem";
 
-function Feedbacklist({feedback, handleDelete}) {
-    if(!feedback || feedback.length === 0){
-        return <p>No Feedback Yet</p>
-    }
+function Feedbacklist({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
 
   return (
     <div className="feedback-list">
-      
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div key={item.id}
+          initial={{opacity:0}}
+          animate={{ opacity: 1}}
+          exit={{opacity: 0}}>
+            
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
-  )
+  );
 }
 
-export default Feedbacklist
+export default Feedbacklist;
