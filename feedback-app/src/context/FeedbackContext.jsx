235ae3df4c72cea57b62bb5db69364de3e7672b2
Ramy Mohamed <ramy.opene@gmail.com>
2023-03-27 07:46:32 -0400

    connect to local database

--- feedback-app/src/context/FeedbackContext.jsx
+++ feedback-app/src/context/FeedbackContext.jsx
@@ -1,50 +1,54 @@
 import { v4 as uuidv4 } from "uuid";
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "this item is from context1",
-      rating: 6,
-    },
-  ]);
-
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
-  })
+  });
 
-const deleteFeedback = (id) => {
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:8000/feedback?_sort=id&_order=desc`
+    );
+    const data = await response.json();
+
+    setFeedback(data);
+  };
+
+  const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
-const addFeedback = (newFeedback) => {
+  const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
 
-  const updateFeedbackItem = (id, updItem)=>{
+  const updateFeedbackItem = (id, updItem) => {
     // console.log(id, updItem)
     setFeedback(
-        feedback.map((item)=>(item.id===id ? {...item,...updItem }: item))
-        )
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
 
-        setFeedbackEdit({
-          item: {},
-          edit: false
-        })
-}
+    setFeedbackEdit({
+      item: {},
+      edit: false,
+    });
+  };
 
   const editFeedback = (item) => {
     setFeedbackEdit({ item, edit: true });
-  }
+  };
   return (
     <FeedbackContext.Provider
       value={{
@@ -54,7 +58,6 @@ const addFeedback = (newFeedback) => {
         addFeedback,
         editFeedback,
         updateFeedbackItem,
-        
       }}
     >
       {children}
